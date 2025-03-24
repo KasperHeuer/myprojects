be0145ca32e8fb31d6fd049678dbdc7b105af85f
KasperHeuer <kasper100@ziggo.nl>
2025-03-24 10:49:14 +0100

    started

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Select</title>
+</head>
+
+<body>
+    <div class="container">
+        <a href="temp.php" class="Link" id="Temp">Temprature converters</a>
+    </div>
+</body>
+
+</html>
